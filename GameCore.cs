61e1c74c11290b1b3053698f2c1626e668c87dd9
he01409918 <92556503+he01409918@users.noreply.github.com>
2022-04-28 11:29:47 +0800

    Update GameCore.cs

--- GameCore.cs
+++ GameCore.cs
@@ -1,13 +1,27 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameCore : MonoBehaviour
 {
     public static GameCore Instance;
+    [Header("生玩家位置")]
     public Transform player;
+    [Header("生成殭屍Prefab")]
+    public GameObject zombie;
+    [Header("生成殭屍的座標")]
+    public Transform[] zombiePoint;
+    [Header("生成殭屍間隔")]
+    public float nextZombieTime = 5;
     private void Start()
     {
         Instance = this;
+        InitZombie();
     }
+    public void InitZombie()
+    {
+        GameObject newZombie = Instantiate(zombie, zombiePoint[Random.Range(0, zombiePoint.Length)].position, zombie.transform.rotation);
+        Invoke(nameof(InitZombie), nextZombieTime);
+    }
+
 }
