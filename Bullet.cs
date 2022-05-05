4b835b19fc8098f20ddef09e8e75d947a9db5cc4
he01409918 <92556503+he01409918@users.noreply.github.com>
2022-05-05 11:52:59 +0800

    Add files via upload

---
+++ Bullet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [Header("爆炸特效")]
+    public GameObject effect;
+    private void OnCollisionEnter(Collision collision)
+    {
+        Instantiate(effect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
