b965eaf6df08469fd75cdd1ab3205af1041c9d10
he01409918 <92556503+he01409918@users.noreply.github.com>
2022-05-05 09:50:04 +0800

    Update Zombie.cs

--- Zombie.cs
+++ Zombie.cs
@@ -1,24 +1,35 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Zombie : MonoBehaviour
 {
+    private Animator animator;
     private NavMeshAgent nav;
 
     void Start()
     {
         nav = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         Movement();
+        CheckAnimator();
     }
 
     private void Movement()
     {
         nav.SetDestination(GameCore.Instance.player.position);
+    }
+    private void CheckAnimator()
+    {
+        float distance = Vector3.Distance(transform.position, GameCore.Instance.player.position);
+        if (distance <= nav.stoppingDistance)
+        {
+            animator.SetBool("Attack", true);
+        }
     }
 }
