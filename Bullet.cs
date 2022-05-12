5f57cd1d78698f79c08b4530b65a65be4ab03a57
he01409918 <92556503+he01409918@users.noreply.github.com>
2022-05-12 10:04:25 +0800

    Update Bullet.cs

--- Bullet.cs
+++ Bullet.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,9 +6,15 @@ public class Bullet : MonoBehaviour
 {
     [Header("爆炸特效")]
     public GameObject effect;
+    [Header("傷害")]
+    public float damage = 50;
     private void OnCollisionEnter(Collision collision)
     {
         Instantiate(effect, transform.position, transform.rotation);
         Destroy(gameObject);
+        if (collision.collider.CompareTag("Zombie"))
+        {
+            collision.collider.GetComponent<Zombie>().GetHit(damage);
+        }
     }
 }
